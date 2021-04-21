5ae255877788f776e05aa255a9f90f6d93a426d9
meekimobile <meekimobile@gmail.com>
2021-04-21 07:53:55 +0700

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://meekimobile.github.io/pxt-esp01-wifi-module/](https://meekimobile.github.io/pxt-esp01-wifi-module/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/meekimobile/pxt-esp01-wifi-module** and import
+
+## Edit this project ![Build status badge](https://github.com/meekimobile/pxt-esp01-wifi-module/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/meekimobile/pxt-esp01-wifi-module** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/meekimobile/pxt-esp01-wifi-module/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,114 @@
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+/**
+ * WiFi with Blynk blocks
+ * 
+ * ESP01 must be ready with AT v1.7.4.
+ * 
+ * ESP01 <--> microbit
+ * 3V3 <--> 3V3
+ * GND <--> GND
+ * RX <--> P1
+ * TX <--> P2
+ * 
+ * ESP01 DIY header <--> IOBit
+ * Put adapter on P1,P2 (IOBit)
+ * 
+ * NOTE:
+ * - ESP01 AT firmware doesn't support SSL.
+ * - WiFi extension doesn't officially supported Blynk. Always use Virtual Pin on Blynk.
+ */
+//% weight=100 color=#0fbc11 icon="\uf1eb" block="WiFi Module"
+namespace WifiModule {
+    let response:string = ""
+    let newLine:string = "\r" + "\n"
+    let is_connected:boolean = false
+    let is_busy:boolean = false
+
+    function executeAtCommand(command: string, waitMs: number) {
+        serial.writeString("" + command + newLine)
+        basic.pause(waitMs)
+    }
+
+    /**
+     * Setup and connect to WiFi.
+     * @param ssid WiFi SSID
+     * @param password Password
+     */
+    //% block="Connect WiFi with|RxPin %rxPin|TxPin %txPin|SSID %ssid|Password %passsword"
+    export function setupWifi(rxPin: SerialPin, txPin: SerialPin, ssid: string, password: string) {
+        serial.redirect(rxPin, txPin, BaudRate.BaudRate115200)
+        serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+            let data: string;
+            
+            let chunk = serial.readString()
+            if (chunk.includes("[")) {
+                data = chunk.slice(chunk.indexOf("["))
+                response = response + data
+            } else if (chunk.includes("]")) {
+                data = chunk
+                response = response + data
+            }            
+        })
+        executeAtCommand("AT+RESTORE", 1000)
+        executeAtCommand("AT+RST", 1000)
+        executeAtCommand("AT+CWMODE=1", 1000)
+        executeAtCommand("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"", 3000)
+        is_connected = true
+    }
+
+    /**
+     * Is WiFi connected? 
+     */
+    //% block
+    export function isConnected(): boolean {
+        return is_connected
+    }
+
+    /**
+     * Read pin's value from Blynk.
+     * @param blynkKey Token from Blynk
+     * @param pin Pin on Blynk
+     */
+    //% block="Read from Blynk with|Token %blynkKey|Pin %pin"
+    export function readBlynkPinValue(blynkKey: string, pin: string): string {        
+        if (is_busy) {
+            return ""
+        }
+        is_busy = true
+        response = ""
+        executeAtCommand("AT+CIPSTART=\"TCP\",\"blynk-cloud.com\",80", 1000)
+        let command:string = "GET /" + blynkKey + "/get/" + pin + " HTTP/1.1" + newLine + "Host: blynk-cloud.com" + newLine + newLine
+        executeAtCommand("AT+CIPSEND=" + ("" + command.length), 0)
+        executeAtCommand(command, 1000)
+        let v = response.slice(response.indexOf("[") + 2, response.indexOf("]") - 1) // Extract value
+        executeAtCommand("AT+CIPCLOSE", 1000)
+        is_busy = false
+        return v
+    }
+
+    /**
+     * Write pin's value to Blynk.
+     * @param blynkKey Token from Blynk
+     * @param pin Pin on Blynk
+     * @param value Value
+     */
+    //% block="Write to Blynk with|Token %blynkKey|Pin %pin|Value %value"
+    export function writeBlynkPinValue(blynkKey: string, pin: string, value: string) {
+        if (is_busy) {
+            return
+        }
+        is_busy = true
+        response = ""
+        executeAtCommand("AT+CIPSTART=\"TCP\",\"blynk-cloud.com\",80", 1000)
+        let command:string = "GET /" + blynkKey + "/update/" + pin + "?value=" + ("" + value) + " HTTP/1.1" + newLine + "Host: blynk-cloud.com" + newLine + newLine
+        executeAtCommand("AT+CIPSEND=" + ("" + command.length), 0)
+        executeAtCommand(command, 1000)
+        executeAtCommand("AT+CIPCLOSE", 1000)
+        is_busy = false
+    }
+
+}
