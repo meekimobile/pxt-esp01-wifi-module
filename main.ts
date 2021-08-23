624e18a2c0d3342d0f5c679574ad3c6e3e0632f5
meekimobile <meekimobile@gmail.com>
2021-08-23 14:48:39 +0700

    fix waiting for response, rxBuffer = 64

--- main.ts
+++ main.ts
@@ -23,9 +23,11 @@
  */
 //% weight=100 color=#0fbc11 icon="\uf1eb" block="WiFi Module"
 namespace WifiModule {
+    let debugging: boolean = false
     let response: string = ""
     let is_connected: boolean = false
     let is_busy: boolean = false
+    let expectedLength = 0
 
     /**
      * Execute AT command
@@ -33,13 +35,17 @@ namespace WifiModule {
      * @param waitMs Waiting in milliseconds
      */
     //% block="AT command|Command %command|Wait in ms %waitMs"
-    export function executeAtCommand(command: string, waitMs: number) {
+    export function executeAtCommand(command: string, waitMs: number, ignoreResponse: boolean = false) {
         let newLine: string = "\r\n"
         if (isConnected) {
             let timeout = 15000 // in ms
             response = ""
+            expectedLength = 0
             is_busy = true
             serial.writeString("" + command + newLine)
+            if (ignoreResponse) {
+                is_busy = false
+            }
             while (is_busy && timeout > 0) {
                 basic.pause(100)
                 timeout = timeout - 100
@@ -47,6 +53,37 @@ namespace WifiModule {
             basic.pause(waitMs)
         } else {
             basic.showIcon(IconNames.No)
+        }
+    }
+
+    function readFromWiFi() {
+        // let chunk = serial.readString()
+        let chunk = serial.readBuffer(0)   // try read all available bytes.
+        //let chunk = serial.readUntil(serial.delimiters(Delimiters.NewLine))   // not work
+
+        if (chunk.length == 0) {
+            return
+        }
+        // basic.showString("c" + chunk.length + ":" + chunk.slice(chunk.length-3).toString() + ".")
+        response = response + chunk.toString()
+
+        if (response.includes("\nOK")) {
+            is_busy = false
+        } else if (response.includes("\nFAIL")) {
+            is_busy = false
+        } else if (response.includes("SEND OK")) {
+            response = response.substr(response.indexOf("SEND OK") + 8)
+        } else if (response.includes("SEND FAIL")) {
+            response = response.substr(response.indexOf("SEND OK") + 8)
+        } else if (response.includes("ERROR")) {
+            is_busy = false
+        } else if (response.includes("+IPD,")) {
+            let start = response.indexOf("+IPD,") + 5
+            let end = response.indexOf(":", start)
+            expectedLength = parseInt(response.substr(start, end-start))            
+            response = response.substr(end + 1)
+        } else if (expectedLength > 0 && response.length >= expectedLength) {
+            is_busy = false
         }
     }
 
@@ -58,31 +95,10 @@ namespace WifiModule {
     //% block="Connect WiFi with|RxPin %rxPin|TxPin %txPin|SSID %ssid|Password %passsword"
     export function connectWifi(rxPin: SerialPin, txPin: SerialPin, ssid: string, password: string) {
         serial.redirect(rxPin, txPin, BaudRate.BaudRate115200)
-        serial.setRxBufferSize(32)
-        serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {       
-            let chunk = serial.readString()
-            //let chunk = serial.readUntil(serial.delimiters(Delimiters.NewLine))   // not work
-            
-            //basic.showString("c" + chunk.length)
-            response = response + chunk
-
-            if (response.includes("\nOK")) {
-                is_busy = false
-            } else if (response.includes("\nFAIL")) {
-                is_busy = false
-            } else if (response.includes("SEND OK")) {
-                response = response.substr(response.indexOf("SEND OK") + 8)
-            } else if (response.includes("SEND FAIL")) {
-                response = response.substr(response.indexOf("SEND OK") + 8)
-            } else if (response.includes("ERROR")) {
-                is_busy = false
-            } else if (response.includes("]")) {
-                is_busy = false
-            }
-        })
-
-        executeAtCommand("AT+RESTORE", 1000)
-        executeAtCommand("AT+RST", 1000)
+        serial.setRxBufferSize(64)   // max RX buffer
+        serial.onDataReceived(serial.delimiters(Delimiters.NewLine), readFromWiFi)
+        executeAtCommand("AT+RESTORE", 1000, true)   // it also reset.
+        // executeAtCommand("AT+RST", 1000, true)   // it doesn't reset WiFi mode.
         executeAtCommand("AT+CWMODE=1", 1000)
         executeAtCommand("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"", 1000)
         if (response.includes("WIFI GOT IP")) {
@@ -97,6 +113,14 @@ namespace WifiModule {
     export function disconnectWifi() {
         executeAtCommand("AT+CWQAP", 2000)
         serial.redirectToUSB()
+    }
+
+    /**
+     * Set debugging mode.
+     */
+    //% block
+    export function setDebugging(b: boolean) {
+        debugging = b;
     }
 
     /**
@@ -135,9 +159,11 @@ namespace WifiModule {
         let command: string = "GET /" + blynkKey + "/get/" + pin + " HTTP/1.1" + newLine + "Host: blynk-cloud.com" + newLine + newLine
         executeAtCommand("AT+CIPSEND=" + ("" + command.length), 0)
         executeAtCommand(command, 1000)
-        // ISSUE: Missing chunk
-        // basic.showString("r" + response.length + ":" + response.substr(response.length - 3))
-        let v = ""
+        if (debugging) {
+            basic.showString("c" + response.length + ":" + response.substr(response.length - 3) + ".")
+            basic.pause(1000)
+        }
+        let v = "?"
         if (response.indexOf("]") >= 0) {
             v = response.slice(response.indexOf("[") + 2, response.indexOf("]") - 1) // Extract value
         }
--- test.ts
+++ test.ts
@@ -1,19 +1,27 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-WifiModule.connectWifi(SerialPin.P2, SerialPin.P1, "mSPACE", "18871008")
-if (WifiModule.isConnected()) {
-    basic.showIcon(IconNames.Square)
-    basic.pause(1000)
-    let v = WifiModule.readBlynkPinValue("3ddvJKfwOGaI551ooPg05YeKgDALMtj9", "V1")
-    basic.showString(v)
+
+function loop() {
+    if (WifiModule.isConnected()) {
+        basic.showIcon(IconNames.Square)
+        basic.pause(1000)
+        readFromBlynk()
+    } else {
+        basic.showIcon(IconNames.SmallSquare)
+        WifiModule.connectWifi(SerialPin.P2, SerialPin.P1, "MeekiSam", "Maijung04") // Work well
+        // WifiModule.connectWifi(SerialPin.P2, SerialPin.P1, "mSPACE", "18871008")    // Not work
+        WifiModule.setDebugging(true)
+    }
+    basic.pause(5000)
 }
 
-input.onButtonPressed(Button.B, function() {
+basic.forever(loop)
+
+function readFromBlynk() {
     basic.showArrow(ArrowNames.South)
     let v = WifiModule.readBlynkPinValue("3ddvJKfwOGaI551ooPg05YeKgDALMtj9", "V1")
     // ISSUE: Not stable. Sometimes got data, sometimes not.
+    // WORKAROUND: It seems fixed after change RX buffer = 64.
     basic.showString(v)
-})
+}
 
-input.onButtonPressed(Button.A, function() {
-    WifiModule.disconnectWifi()
-})
+input.onButtonPressed(Button.B, readFromBlynk)
