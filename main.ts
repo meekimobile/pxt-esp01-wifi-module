e7f6b0a35c5cce86ea4bf91773c788fdd4d2b53f
meekimobile <meekimobile@gmail.com>
2022-05-20 14:31:01 +0700

    add NTP blocks

--- main.ts
+++ main.ts
@@ -29,6 +29,10 @@ namespace WifiModule {
     let is_busy: boolean = false
     let expectedLength = 0
 
+    let internetTimeInitialized = false
+    let internetTimeUpdated = false
+    let internetTime: string[] = []
+
     /**
      * Execute AT command
      * @param command AT command
@@ -231,6 +235,79 @@ namespace WifiModule {
         }
         executeAtCommand("AT+CIPCLOSE", 1000)
         return v
+    }
+
+    /**
+     * Initialize the internet time.
+     * @param timezone Timezone. eg: 8
+     * @param ntpUrl NTP server URL. eg: pool.ntp.org
+     */
+    //% block="Initialize internet time at |TimeZone %timezone|NTP server URL %ntpUrl"
+    //% timezone.min=-11 timezone.max=13
+    export function initInternetTime(timezone: number, ntpUrl: string) {
+        internetTimeInitialized = false
+        internetTimeUpdated = false
+
+        if (!ntpUrl) ntpUrl =  "pool.ntp.org"
+        if (!isConnected) return
+
+        executeAtCommand("AT+CIPSNTPCFG=1," + timezone + ",\"" + ntpUrl + "\"", 500)
+
+        internetTimeInitialized = true
+    }
+
+    /**
+     * Update the internet time.
+     */
+    //% block="Update internet time"
+    export function updateInternetTime() {
+        internetTimeUpdated = false
+
+        if (!isConnected) return
+        if (!isInternetTimeInitialized) return
+
+        executeAtCommand("AT+CIPSNTPTIME?", 2000)
+        if (getResponse() != "") {
+            let response = getResponse()
+            let dtArray = response.slice(response.indexOf(":") + 1, response.indexOf("\nOK")).split(" ")
+            let timeArray = dtArray[3].split(":")
+            if (dtArray[4].trim() == "1970") {
+                return
+            }
+            internetTime = [
+                dtArray[4].trim(),
+                dtArray[1],
+                dtArray[2],
+                timeArray[0],
+                timeArray[1],
+                timeArray[2]
+            ]
+            internetTimeUpdated = true
+        }
+    }
+
+    /**
+     * Return true if the internet time is initialzed successfully.
+     */
+    //% block="Is internet time initialized"
+    export function isInternetTimeInitialized(): boolean {
+        return internetTimeInitialized
+    }
+
+    /**
+     * Return true if the internet time is updated successfully.
+     */
+    //% block="Is internet time updated"
+    export function isInternetTimeUpdated(): boolean {
+        return internetTimeUpdated
+    }
+
+    /**
+     * Return internet time array as [year, month, day, hour, minute, second].
+     */
+    //% block="Get internet time array"
+    export function getInternetTimeArray(): string[] {
+        return internetTime
     }
 
 }
--- test.ts
+++ test.ts
@@ -1,57 +1,28 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-let success = 0
-let count = 0
-
-function loop() {
-    if (WifiModule.isConnected()) {
-        let v = readFromBlynk()
-        // writeToBlynk()
-        count++
-        if (v != "?") {
-            success++
-        }
-        basic.pause(2000)
-        // led.plotBarGraph(success, count)   // Don't use, it writes number on UART.
-        plotDotGraph(success, count, 10)
-        basic.pause(8000)
-    } else {
-        basic.showIcon(IconNames.SmallSquare)
-        WifiModule.connectWifi(SerialPin.P2, SerialPin.P1, "mSPACE", "18871008")
-        // WifiModule.setDebugging(true)
-        if (WifiModule.isConnected()) {
-            basic.showIcon(IconNames.Square)
-        } else {
-            basic.showIcon(IconNames.No)
-        }
-        basic.pause(5000)
+function onBpressed() {
+    basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+    WifiModule.updateInternetTime()
+    if (WifiModule.isInternetTimeUpdated()) {
+        basic.showString(WifiModule.getInternetTimeArray().join(","))
     }
+    basic.pause(1000)
 }
+input.onButtonPressed(Button.B, onBpressed)
 
-basic.forever(loop)
-
-function plotDotGraph(v:number, max:number, dots:number = 25) {
-    let s = Math.floor(Math.map(v, 0, max, 0, dots))
-    basic.clearScreen()
-    for (let i = 0; i < s; i++) {
-        led.plot(i % 5, Math.floor(i / 5))
-    }
-}
-
-function readFromBlynk() : string {
-    basic.showArrow(ArrowNames.South)
-    let v = WifiModule.readBlynkPinValue("3ddvJKfwOGaI551ooPg05YeKgDALMtj9", "V1")
-    // ISSUE: Not stable. Sometimes got data, sometimes not. How to always empty RX buffer?
-    /* WORKAROUND: 
-     * - It seems fixed after change RX buffer = 64.
-     * - Use AT firmware V2.1
-    */
-    basic.showString(v)
-    return v
-}
-
-function writeToBlynk() {
-    basic.showArrow(ArrowNames.North)
-    WifiModule.writeBlynkPinValue("3ddvJKfwOGaI551ooPg05YeKgDALMtj9", "V4", ""+input.lightLevel())
-}
-
-input.onButtonPressed(Button.B, writeToBlynk)
+basic.showIcon(IconNames.SmallSquare)
+WifiModule.connectWifi(SerialPin.P2, SerialPin.P1, "mSPACE", "18871008")
+basic.showIcon(IconNames.Square)
+WifiModule.initInternetTime(7, undefined)
+basic.showLeds(`
+. # # # .
+# . # . #
+# . # # #
+# . . . #
+. # # # .
+`)
