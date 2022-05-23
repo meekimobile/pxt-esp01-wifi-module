a771e2d780830467736393a4b202f74c06ad9adc
meekimobile <meekimobile@gmail.com>
2022-05-23 17:14:39 +0700

    add mqtt (wip)

--- test.ts
+++ test.ts
@@ -15,6 +15,34 @@ function onBpressed() {
 }
 input.onButtonPressed(Button.B, onBpressed)
 
+function mqttPub() {
+    let cmd
+    cmd = 'AT+MQTTUSERCFG=0,1,"microbit_esp01_1","ktaweesak","aio_GUUC68Zjxg8lFmoO7UsHyk7b1OWu",0,0,""'
+    WifiModule.executeAtCommand(cmd, 1000)
+    cmd = 'AT+MQTTCONN=0,"io.adafruit.com",1883,0'
+    WifiModule.executeAtCommand(cmd, 1000)
+    cmd = 'AT+MQTTPUB=0,"ktaweesak/feeds/v0","' + input.lightLevel() + '",0,0'
+    WifiModule.executeAtCommand(cmd, 1000)
+    cmd = 'AT+MQTTCLEAN=0'
+    WifiModule.executeAtCommand(cmd, 1000)
+}
+function mqttSub() {
+    let cmd
+    cmd = 'AT+MQTTUSERCFG=0,1,"microbit_esp01_1","ktaweesak","aio_GUUC68Zjxg8lFmoO7UsHyk7b1OWu",0,0,""'
+    WifiModule.executeAtCommand(cmd, 1000)
+    cmd = 'AT+MQTTCONN=0,"io.adafruit.com",1883,0'
+    WifiModule.executeAtCommand(cmd, 1000)
+    cmd = 'AT+MQTTSUB=0,"ktaweesak/feeds/v1",0'
+    WifiModule.executeAtCommand(cmd, 1000)
+}
+function mqttClean() {
+    let cmd
+    cmd = 'AT+MQTTCLEAN=0'
+    WifiModule.executeAtCommand(cmd, 1000)
+}
+input.onButtonPressed(Button.A, mqttPub)
+input.onButtonPressed(Button.AB, mqttClean)
+
 basic.showIcon(IconNames.SmallSquare)
 WifiModule.connectWifi(SerialPin.P2, SerialPin.P1, "mSPACE", "18871008")
 basic.showIcon(IconNames.Square)
